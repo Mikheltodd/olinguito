52f67f1de39c9fbf9fc05fcfcb3b8d5ce27a19b7
Mikheltodd <68166999+Mikheltodd@users.noreply.github.com>
2020-12-21 09:33:57 -0500

    Precios Iniciales en cero

--- db/hotel_db.py
+++ db/hotel_db.py
@@ -22,23 +22,23 @@ database_hotels = {
                               "n_rooms": 25,
                               "total_operation_cost": 450000,
                               "l_days": 120,
-                              "h_days": 100, "l_price": 1,
-                              "m_price": 2,
-                              "h_price": 3}),
+                              "h_days": 100, "l_price": 0,
+                              "m_price": 0,
+                              "h_price": 0}),
     "Hotel1": HotelInDB(**{"name": "Hotel1",
                            "n_rooms": 20,
                            "total_operation_cost": 300000,
                            "l_days": 100,
-                           "h_days": 80, "l_price": 1,
-                           "m_price": 2,
-                           "h_price": 3}),
+                           "h_days": 80, "l_price": 0,
+                           "m_price": 0,
+                           "h_price": 0}),
     "Hotel2": HotelInDB(**{"name": "Hotel2",
                            "n_rooms": 30,
                            "total_operation_cost": 600000,
                            "l_days": 150,
-                           "h_days": 90, "l_price": 1,
-                           "m_price": 2,
-                           "h_price": 3}),
+                           "h_days": 90, "l_price": 0,
+                           "m_price": 0,
+                           "h_price": 0}),
 }
 
 
@@ -48,17 +48,21 @@ def get_hotel_info(hotel_name: str):
     else:
         return None
 
-def create_hotel(hotel_in:HotelInDB):
+
+def create_hotel(hotel_in: HotelInDB):
     if hotel_in.name in database_hotels:
         return False
     else:
-        database_hotels[hotel_in.name]= hotel_in
+        database_hotels[hotel_in.name] = hotel_in
         return True
+
 
 def update_hotel(hotel_in_db: HotelInDB):
     database_hotels[hotel_in_db.name] = hotel_in_db
     return hotel_in_db
 
 # Devuelve los valores de lista de hoteles
+
+
 def get_all_hotels():
     return database_hotels.values()
